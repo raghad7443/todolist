55330f7351009fd530b661d917502ae33f06befc
raghad <r22160733@gmail.com>
2024-07-23 07:52:28 +0300

    header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,24 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+import dayjs from 'dayjs';
+ import 'dayjs/locale/ar';
+ dayjs.locale('ar');
+</script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
+
+<div class="p-16">
+
+	<header class=" flex justify-between">
+		<div>
+			<h1 class=" text-4xl mb-3"> <!-- put the day and the greeting messge in the same div-->
+				صباح الخير👋
+			   </h1>
+			   <h2 class=" text-2xl text-surface-800/70">
+				{dayjs().format('اليوم ,dddd D MMM YYYY')}
+			   </h2>
+		</div>
+		<div class="bg-slate-500 h-14 w-32 ">
+
+		</div>
+		</header>
+
 </div>
