668e4b4ecb1792890e4fad7cb798eed1782cdb81
raghad <r22160733@gmail.com>
2024-07-25 21:31:41 +0300

    HEADER COMP

---
+++ src/lib/Header.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import dayjs from "dayjs";
+  import "dayjs/locale/ar";
+  dayjs.locale("ar");
+
+  let period = dayjs().format("a") == "م" ? "مساء" : " صباح";
+</script>
+<!-- the ? means if and : means else/////// so i read it like if this is true then do   else do that -->
+<header class=" flex justify-between">
+  <div>
+    <h1 class=" text-4xl mb-3">
+      <!-- put the day and the greeting messge in the same div-->
+      {period} الخير👋
+    </h1>
+    <h2 class=" text-2xl text-surface-800/70">
+      {dayjs().format("اليوم ,dddd D MMM YYYY")}
+    </h2>
+  </div>
+  <div class="bg-slate-200 h-14 w-32"></div>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,9 @@
 <script lang="ts">
-import dayjs from 'dayjs';
- import 'dayjs/locale/ar';
- dayjs.locale('ar');
+import Header from "$lib/Header.svelte";
 
- let period=dayjs().format("a")=="م" ? "مساء" : " صباح"; 
-</script> 
+</script>
 
-<!-- the ? means if and : means else/////// so i read it like if this is true then do   else do that -->
+
 <div class="p-16">
-
-	<header class=" flex justify-between">
-		<div>
-			<h1 class=" text-4xl mb-3"> <!-- put the day and the greeting messge in the same div-->
-				{period} الخير👋
-			   </h1>
-			   <h2 class=" text-2xl text-surface-800/70">
-				{dayjs().format('اليوم ,dddd D MMM YYYY')}
-			   </h2>
-		</div>
-		<div class="bg-slate-500 h-14 w-32 ">
-
-		</div>
-		</header>
-
+<Header/> 
 </div>
